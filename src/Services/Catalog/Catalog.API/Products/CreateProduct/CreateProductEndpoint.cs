94a79af68a7c2a8ea86369b0bb3050750b17fc79
mamagarage <m.mangiamusardo@gmail.com>
2026-05-28 11:54:20 +0200

    Added integration for PostgreSQL and docker compose file

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,41 +1,36 @@
-﻿using Carter;
-using MediatR;
-using Mapster;
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public Guid Id;
 
-    public record CreateProductResponse(Guid Id);
-
-    public class CreateProductEndpoint : ICarterModule
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
-        public Guid Id;
-
-        public void AddRoutes(IEndpointRouteBuilder app)
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
         {
-            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
-            {
-                //var command = new CreateProductCommand(
-                //    request.Name,
-                //    request.Category,
-                //    request.Description,
-                //    request.ImageFile,
-                //    request.Price
-                //);
+            //var command = new CreateProductCommand(
+            //    request.Name,
+            //    request.Category,
+            //    request.Description,
+            //    request.ImageFile,
+            //    request.Price
+            //);
                 
-                var command = request.Adapt<CreateProductCommand>();
+            var command = request.Adapt<CreateProductCommand>();
 
-                CreateProductResult createProductResult = await sender.Send(command);
+            CreateProductResult createProductResult = await sender.Send(command);
 
-                var createProductResponse = createProductResult.Adapt<CreateProductResponse>();
+            var createProductResponse = createProductResult.Adapt<CreateProductResponse>();
 
-                return Results.Created($"/products/{createProductResponse.Id}", createProductResponse);
-            })
-            .WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-            .WithSummary("Create Product")
-            .WithDescription("Create Product");
-        }
+            return Results.Created($"/products/{createProductResponse.Id}", createProductResponse);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => 
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+// Add Marten
+// Note: Ensure that the connection string "DataBase" is defined in your appsettings.json or environment variables
+// Example connection string in appsettings.json:
+// "ConnectionStrings": {
+//     "DataBase": "Host=localhost;Port=5432;Database=CatalogDb;Username=postgres;Password=yourpassword"
+// }
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DataBase"));
+    //options.AutoCreateSchemaObjects = 
+}).UseLightweightSessions(); // UseLightweightSessions is used to configure Marten to use lightweight sessions, which are optimized for read operations and do not track changes to documents. This can improve performance for read-heavy workloads.
 
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
+app.MapCarter();
+
 
 app.Run();
